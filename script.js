9f90e4a22b2dc0a963eebdb28cba28d64fe10990
abdellahak <93621937+abdellahak@users.noreply.github.com>
2024-05-20 10:13:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll('.card');
+
+
+cards.forEach((card) => {
+    card.addEventListener('click',()=>{
+        shrink();
+        card.classList.add('active');
+    });
+});
+
+
+
+function shrink(){
+    cards.forEach(card => {
+        card.classList.remove('active');
+    });
+};
